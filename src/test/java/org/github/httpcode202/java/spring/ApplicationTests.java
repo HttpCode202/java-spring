62fae242eee60cddd1d6f045ec489c437bb93312
HttpCode202 <neant69neant89@gmail.com>
2026-04-06 17:10:09 +0200

    feat: :sparkles: create spring project

---
+++ src/test/java/org/github/httpcode202/java/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.httpcode202.java.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
